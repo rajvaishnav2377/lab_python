806d81ed79e3813c34c5bd73562672e43ebc04e9
Vaishnav Raj <rajvaishnav2377@gmail.com>
2025-12-31 08:13:05 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,54 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x=10
+print(x)
+10
+a=5
+print(-a)
+-5
+p=10
+q=5
+print(p>q)
+True
+print(p==q)
+False
+x=true
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    x=true
+NameError: name 'true' is not defined. Did you mean: 'True'?
+x="true"
+y="false"
+print(x and y)
+false
+print(x or y)
+true
+print(not x)
+False
+is_pass = true
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    is_pass = true
+NameError: name 'true' is not defined. Did you mean: 'True'?
+print(is_pass)
+Traceback (most recent call last):
+  File "<pyshell#15>", line 1, in <module>
+    print(is_pass)
+NameError: name 'is_pass' is not defined
+a=5
+b=3
+print(a & b)
+1
+print(a | b)
+7
+numbers = [1,2,3,4,6]
+print(2 in numbers)
+True
+print(5 not in numbers)
+True
+x=10
+y=10
+print(x is y)
+True
+print(x is not y)
+False
