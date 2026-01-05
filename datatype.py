2015d0873bf8c3c991fedbb162770b51078da346
Vaishnav Raj <rajvaishnav2377@gmail.com>
2026-01-05 11:11:52 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,43 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x="vaishnav raj"
+print(x)
+vaishnav raj
+x=20
+print(x)
+20
+x=20.77
+print(x)
+20.77
+x=2j
+print(x)
+2j
+x=["manav","dev","meet"]
+print(x)
+['manav', 'dev', 'meet']
+x=("apple","banana","cherry")
+print(x)
+('apple', 'banana', 'cherry')
+x=range(7)
+print(list(x))
+[0, 1, 2, 3, 4, 5, 6]
+x={"name":"raj","age":"19"}
+print(x)
+{'name': 'raj', 'age': '19'}
+x={"book","pan","pencil"}
+print(x)
+{'pencil', 'book', 'pan'}
+x = true
+Traceback (most recent call last):
+  File "<pyshell#18>", line 1, in <module>
+    x = true
+NameError: name 'true' is not defined. Did you mean: 'True'?
+x="true"
+print(x)
+true
+x=b"hello"
+print(x)
+b'hello'
+x=frozenset({"apple","banana","cherry"})
+print(x)
+frozenset({'apple', 'cherry', 'banana'})
