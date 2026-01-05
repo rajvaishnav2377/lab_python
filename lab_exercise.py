ca396fa1c8485750f169c154e91960dcf89ca8bb
Vaishnav Raj <rajvaishnav2377@gmail.com>
2026-01-05 11:32:41 +0530

    Add files via upload

---
+++ lab_exercise.py
@@ -0,0 +1,29 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x="vaishnav raj"
+print(x)
+vaishnav raj
+x=23
+print(x)
+23
+x="true"
+print(x)
+true
+x=("apple","banana","cherry")
+print(x)
+('apple', 'banana', 'cherry')
+x=["apple","banana","cherry"]
+print(x)
+['apple', 'banana', 'cherry']
+x={"name":"raj","age":"19"}
+print(x)
+{'name': 'raj', 'age': '19'}
+x=3j
+print(x)
+SyntaxError: multiple statements found while compiling a single statement
+x=2j
+print(x)
+2j
+x=23.77
+print(x)
+23.77
