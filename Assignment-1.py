049f313551bf47d4161568bab29e45c69409d2eb
Vaishnav Raj <rajvaishnav2377@gmail.com>
2026-01-12 11:40:57 +0530

    Add files via upload

---
+++ Assignment-1.py
@@ -0,0 +1,74 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x="welcome message"
+print(x)
+welcome message
+x="raj vaishanv \n\t flat no.101,sunshine apartment \n\t MG road,sector 15 \n\t rajkot \n\t pincode-360004 \n\t india"
+print(x)
+raj vaishanv 
+	 flat no.101,sunshine apartment 
+	 MG road,sector 15 
+	 rajkot 
+	 pincode-360004 
+	 india
+x="\n\t raj vaishanv \n\t flat no.101,sunshine apartment \n\t MG road,sector 15 \n\t rajkot \n\t pincode-360004 \n\t india"
+print(x)
+SyntaxError: multiple statements found while compiling a single statement
+x="\n\t raj vaishanv \n\t flat no.101,sunshine apartment \n\t MG road,sector 15 \n\t rajkot \n\t pincode-360004 \n\t india"
+print(x)
+
+	 raj vaishanv 
+	 flat no.101,sunshine apartment 
+	 MG road,sector 15 
+	 rajkot 
+	 pincode-360004 
+	 india
+x=150
+y=120.50
+print("addition:",x+y)print("subtraction:",a-b)print("multiplication:",a*b)print("division:",a/b)
+SyntaxError: invalid syntax
+print("subtraction:",a-b)
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    print("subtraction:",a-b)
+NameError: name 'a' is not defined
+print("addition:",x+y)print("subtraction:",x-y)print("multiplication:",x*y)print("division:",x/y)
+SyntaxError: invalid syntax
+print("addition:",x+y)
+addition: 270.5
+print("subtraction:",x-y)
+subtraction: 29.5
+print("multiplication:",x*y)
+multiplication: 18075.0
+print("division:",x/y)
+division: 1.2448132780082988
+x="Area of circle=3.14*radius"
+print(x)
+Area of circle=3.14*radius
+x="circum ference of circle=2*pi*radius"
+print(x)
+circum ference of circle=2*pi*radius
+x="simple interest=(p*r*t)/100"
+print(x)
+simple interest=(p*r*t)/100
+x="perimeter of rectangle=2*(length)*(_"
+x="perimeter of rectangle=2*(length*width)"
+print(x)
+perimeter of rectangle=2*(length*width)
+x=
+SyntaxError: invalid syntax
+x="area of reatangle=length*eidth"
+print(x)
+area of reatangle=length*eidth
+x="perimeter of tringle=a+b+c"
+print(x)
+perimeter of tringle=a+b+c
+x="perimeter of reatangle=2*(length+width)"
+print(x)
+perimeter of reatangle=2*(length+width)
+x="Area of square=side*side"
+print(x)
+Area of square=side*side
+x="perimeter of square=4*side"
+print(x)
+perimeter of square=4*side
