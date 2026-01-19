a3f67b1a4f9efe6be1340fc19d038830d8ab1028
Vaishnav Raj <rajvaishnav2377@gmail.com>
2026-01-19 11:24:31 +0530

    Add files via upload

---
+++ exercise-2.py
@@ -0,0 +1,118 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+for i in range(1,6);
+SyntaxError: expected ':'
+for i in range(1,6):
+    print(i)
+
+    
+1
+2
+3
+4
+5
+text="python"
+length=len(text)
+print(length)
+6
+a=10
+b=20
+print(max(a,b))
+20
+print("welcome")
+welcome
+text="python"
+print(text[0])
+p
+p=float(input("enter principal amount"))
+enter principal amount
+Traceback (most recent call last):
+  File "<pyshell#13>", line 1, in <module>
+    p=float(input("enter principal amount"))
+ValueError: could not convert string to float: ''
+p=float(input("enter principal amount :"))
+enter principal amount :1000
+R=float(input("enter rate of interst:"))
+enter rate of interst:5
+T=float(input(input("enter time in years :"))
+
+        T=float(input("enter time in years :"))
+        
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+ T=float(input("enter time in years :"))
+        
+SyntaxError: unexpected indent
+T=float(input("enter time in years :"))
+        
+enter time in years :2
+SI=(P * R * T)/100
+        
+Traceback (most recent call last):
+  File "<pyshell#21>", line 1, in <module>
+    SI=(P * R * T)/100
+NameError: name 'P' is not defined. Did you mean: 'p'?
+ SI = (P * R * T)/100
+        
+SyntaxError: unexpected indent
+si=(p*r*t) / 100
+        
+Traceback (most recent call last):
+  File "<pyshell#23>", line 1, in <module>
+    si=(p*r*t) / 100
+NameError: name 'r' is not defined. Did you mean: 'R'?
+SI = (p * R * T)/100
+        
+print("simple interest =",SI)
+        
+simple interest = 100.0
+Text="python"
+        
+print(Text[-1])
+        
+n
+s = input("enter a string :")
+        
+enter a string :raj
+
+num= int(input("enter a number :"))
+        
+enter a number :-5
+if num > 0:
+        print("positive number")
+        else if num < 0:
+            
+SyntaxError: invalid syntax
+else if num < 0:
+    
+SyntaxError: invalid syntax
+elif num < 0 :
+    
+SyntaxError: invalid syntax
+if num > 0 :
+    print("positive number")
+    elif num < 0:\
+         
+SyntaxError: invalid syntax
+negative number
+SyntaxError: invalid syntax
+a,b,c = 10,20,30
+print(a+b+c)
+60
+num1=float(input("enter first number :"))
+enter first number :5
+num2=float(input("enter second number :"))
+enter second number :10
+sum=num1+num2
+print("the sum of the number is :",sum)
+the sum of the number is : 15.0
+num=float(input("enter a number :"))
+enter a number :2
+if num > 0:
+    print("The number is positive.")
+elif num < 0:
+    print("The number is negative.")
+else :
+    print("The number is zero.")
+
+    
+The number is positive.
