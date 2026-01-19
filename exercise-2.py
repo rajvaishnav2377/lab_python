9c26c4a57885301470cf661d67a4ef97b7728f10
Vaishnav Raj <rajvaishnav2377@gmail.com>
2026-01-19 11:29:43 +0530

    exercise-2.py

--- exercise-2.py
+++ exercise-2.py
@@ -1,7 +1,5 @@
 Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
-for i in range(1,6);
-SyntaxError: expected ':'
 for i in range(1,6):
     print(i)
 
@@ -23,43 +21,16 @@ print("welcome")
 welcome
 text="python"
 print(text[0])
-p
-p=float(input("enter principal amount"))
-enter principal amount
-Traceback (most recent call last):
-  File "<pyshell#13>", line 1, in <module>
-    p=float(input("enter principal amount"))
-ValueError: could not convert string to float: ''
+
 p=float(input("enter principal amount :"))
 enter principal amount :1000
 R=float(input("enter rate of interst:"))
 enter rate of interst:5
-T=float(input(input("enter time in years :"))
-
-        T=float(input("enter time in years :"))
         
-SyntaxError: invalid syntax. Perhaps you forgot a comma?
- T=float(input("enter time in years :"))
-        
-SyntaxError: unexpected indent
 T=float(input("enter time in years :"))
         
 enter time in years :2
-SI=(P * R * T)/100
-        
-Traceback (most recent call last):
-  File "<pyshell#21>", line 1, in <module>
-    SI=(P * R * T)/100
-NameError: name 'P' is not defined. Did you mean: 'p'?
- SI = (P * R * T)/100
-        
-SyntaxError: unexpected indent
-si=(p*r*t) / 100
-        
-Traceback (most recent call last):
-  File "<pyshell#23>", line 1, in <module>
-    si=(p*r*t) / 100
-NameError: name 'r' is not defined. Did you mean: 'R'?
+
 SI = (p * R * T)/100
         
 print("simple interest =",SI)
@@ -74,25 +45,7 @@ s = input("enter a string :")
         
 enter a string :raj
 
-num= int(input("enter a number :"))
-        
-enter a number :-5
-if num > 0:
-        print("positive number")
-        else if num < 0:
-            
-SyntaxError: invalid syntax
-else if num < 0:
-    
-SyntaxError: invalid syntax
-elif num < 0 :
-    
-SyntaxError: invalid syntax
-if num > 0 :
-    print("positive number")
-    elif num < 0:\
-         
-SyntaxError: invalid syntax
+
 negative number
 SyntaxError: invalid syntax
 a,b,c = 10,20,30
@@ -116,3 +69,4 @@ else :
 
     
 The number is positive.
+
