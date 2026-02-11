1b36f2b9ed61ada60f072cdf6870852422d4bdea
Vaishnav Raj <rajvaishnav2377@gmail.com>
2026-02-11 08:26:15 +0530

    Add files via upload

---
+++ conditonal_statement.py
@@ -0,0 +1,31 @@
+"""num=int(input("enter your value :"))
+if num>0:
+    print("positive number")
+else:
+    print("negative number")"""
+    
+"""age=int(input("enter your age :"))
+if age>=18:
+    print("you are eligible to vote")
+else:
+    print("you are not eligible to vote")"""
+    
+"""num=int(input("enter your number:"))
+if num%2==0:
+    print("even number")
+else:
+    print("odd number")"""
+    
+"""marks=int(input("enter your marks:"))
+if marks>=90:
+    print("you will get A grade")
+elif marks>=70:
+    print("you will get B grade")
+else :
+    print("you will get C grade")"""
+    
+age=int(input("enter your age:"))
+if age>=18:
+    print("you are eligible for licence")
+else:
+    print("you are not eligible for licence")
