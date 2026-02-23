d6f2f00296df9afdd0a7b5bf468393eab2874166
Vaishnav Raj <rajvaishnav2377@gmail.com>
2026-02-23 11:29:36 +0530

    Add files via upload

---
+++ io.py
@@ -0,0 +1,16 @@
+a="hello harsh"
+print(a)
+name=input("enter your name:")
+print("hello",name)
+number=int(input("enter your number:"))
+print("hello",number)
+a=40
+print(a)
+number=int(input("enter your number:"))
+print("atmiya",number)
+number=float(input("enter your number"))
+print("atmiya",number)
+a='True'
+b='False'
+print(a)
+
