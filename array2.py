8fa7b7b31bfb9db71447db4f29ffc475d717448e
Vaishnav Raj <rajvaishnav2377@gmail.com>
2026-03-11 08:28:47 +0530

    Add files via upload

---
+++ array2.py
@@ -0,0 +1,18 @@
+#1)
+arr = [10, 20, 30, 40, 50]
+
+print(arr[0]) 
+print(arr[1])  
+print(arr[2])
+
+#2)
+arr = [10, 20, 30, 40, 50]
+
+print(arr[-1])  
+print(arr[-2])  
+print(arr[-3]) 
+
+#3)
+arr = [10, 20, 30]
+arr[1] = 99
+print(arr)
