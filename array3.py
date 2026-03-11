ea90ed29639fd7c7246cfa6b7ef8481c4204125d
Vaishnav Raj <rajvaishnav2377@gmail.com>
2026-03-11 08:29:02 +0530

    Add files via upload

---
+++ array3.py
@@ -0,0 +1,31 @@
+#1)
+from array import array
+arr=array('i',[10,20,30,40,50])
+print(arr[1:4])
+print(arr[:3])
+print(arr[:])
+
+#2)
+from array import array
+arr=array('i',[10,20,30,40,50,60,70,80])
+print(arr[::2])
+print(arr[1::2])
+print(arr[::3])
+
+#3)
+from array import array
+arr=array('i',[10,20,30,40,50])
+print(arr[-4:-1])
+print(arr[-3:])
+print(arr[:-2])
+
+#4)
+from array import array
+arr=array('i',[10,20,30,40,50])
+print(arr[::-1])
+
+#5)
+from array import array 
+arr=array('i',[10,20,30,40,50])
+arr[1:4]=array('i',[25,35,43])
+print(arr)
