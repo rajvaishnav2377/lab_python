228248089c51bb4615a418ac465d45525d0903f7
Vaishnav Raj <rajvaishnav2377@gmail.com>
2026-03-18 07:45:57 +0530

    Add files via upload

---
+++ default.py
@@ -0,0 +1,15 @@
+def sqr(num,exp=2):
+    return num ** exp
+print(sqr(3))
+print(sqr(3,3))
+print(sqr(2,4))
+
+def greet(name = "guest"):
+    print("hello",name)
+greet("alice")
+greet()
+
+def add(a,b=5):
+    print("sum:",a+b)
+    add(10,20)
+    add(10)
